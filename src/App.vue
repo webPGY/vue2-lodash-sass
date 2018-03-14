9eb8dd4c06c234fbbd3aca1d8fe6906f20fed4f4
pengfufu <1102904987@qq.com>
2018-03-14 19:38:33 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import $ from "jquery"
+export default {
+  name: 'App',
+  mounted(){
+
+  }
+}
+</script>
+
+<style>
+
+</style>
